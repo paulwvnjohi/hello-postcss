efb51ec442ea04a79d10785bb20a1838208d1a67
paul wanjohi <paulwvnjohi@gmail.com>
2021-12-22 21:07:42 +0300

    initial commit

---
+++ postcss.config.cjs
@@ -0,0 +1,12 @@
+const postcssPresetEnv = require('postcss-preset-env');
+
+module.exports = {
+	plugins: [
+		postcssPresetEnv({
+			stage: 3,
+			features: {
+				'nesting-rules': true
+			}
+		})
+	]
+};
